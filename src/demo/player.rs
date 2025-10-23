fbd9b526497fa9b17dd1273359b63183b7c6064f
Abigail Moore <abigailmmoore00@gmail.com>
2025-10-23 00:18:31 -0600

    Fixed analog gamepad input

--- src/demo/player.rs
+++ src/demo/player.rs
@@ -72,20 +72,7 @@ fn record_player_directional_input(
     gamepads: Query<&Gamepad>,
     mut controller_query: Query<&mut MovementController, With<Player>>,
 ) {
-    // Collect directional input.
     let mut intent = Vec2::ZERO;
-    if input.pressed(KeyCode::KeyW) || input.pressed(KeyCode::ArrowUp) {
-        intent.y += 1.0;
-    }
-    if input.pressed(KeyCode::KeyS) || input.pressed(KeyCode::ArrowDown) {
-        intent.y -= 1.0;
-    }
-    if input.pressed(KeyCode::KeyA) || input.pressed(KeyCode::ArrowLeft) {
-        intent.x -= 1.0;
-    }
-    if input.pressed(KeyCode::KeyD) || input.pressed(KeyCode::ArrowRight) {
-        intent.x += 1.0;
-    }
 
     // Add gamepad input if available
     if let Some(gamepad_res) = my_gamepad {
@@ -102,10 +89,24 @@ fn record_player_directional_input(
             }
         }
     }
+    else {
+        // Collect directional input.
+        if input.pressed(KeyCode::KeyW) || input.pressed(KeyCode::ArrowUp) {
+            intent.y += 1.0;
+        }
+        if input.pressed(KeyCode::KeyS) || input.pressed(KeyCode::ArrowDown) {
+            intent.y -= 1.0;
+        }
+        if input.pressed(KeyCode::KeyA) || input.pressed(KeyCode::ArrowLeft) {
+            intent.x -= 1.0;
+        }
+        if input.pressed(KeyCode::KeyD) || input.pressed(KeyCode::ArrowRight) {
+            intent.x += 1.0;
+        }
 
-    // Normalize intent so that diagonal movement is the same speed as horizontal / vertical.
-    // This should be omitted if the input comes from an analog stick instead.
-    let intent = intent.normalize_or_zero();
+        // Normalize intent so that diagonal movement is the same speed as horizontal / vertical.
+        let intent = intent.normalize_or_zero();
+    }
 
     // Apply movement intent to controllers.
     for mut controller in &mut controller_query {
@@ -148,7 +149,7 @@ mod gamepad {
     use bevy::prelude::*;
 
     #[derive(Resource)]
-    pub(super) struct MyGamepad(pub(super) Entity); // Make the Entity public
+    pub(super) struct MyGamepad(pub(super) Entity);
 
     pub(super) fn gamepad_connections(
         mut commands: Commands,
