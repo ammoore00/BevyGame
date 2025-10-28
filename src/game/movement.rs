713c4e63bf81e34d22d982aee22a8443cc225737
Abigail Moore <abigailmmoore00@gmail.com>
2025-10-27 18:30:23 -0600

    Rudimentary gravity

--- src/game/movement.rs
+++ src/game/movement.rs
@@ -56,7 +56,7 @@ impl Default for MovementController {
 pub const TILE_BOUNDARY_SIZE: f32 = 0.02;
 pub const STEP_UP_THRESHOLD: f32 = 0.25;
 
-pub const NUM_MOVEMENT_STEPS: usize = 1;
+pub const GRAVITY: f32 = 4.5;
 
 fn apply_movement(
     time: Res<Time>,
@@ -64,88 +64,95 @@ fn apply_movement(
     tile_query: Query<(Entity, &TileCollision)>,
     grid_query: Query<&Grid>,
 ) {
-    for _ in 0..NUM_MOVEMENT_STEPS {
-        for (controller, mut controller_position, collider) in &mut movement_query {
-            if let Ok(grid) = grid_query.single() {
-                let tile_map = grid.0.read().unwrap();
+    for (controller, mut controller_position, collider) in &mut movement_query {
+        if let Ok(grid) = grid_query.single() {
+            let tile_map = grid.0.read().unwrap();
 
-                let velocity = controller.intent * controller.max_speed;
-                let velocity = if velocity.x.abs() < 0.01 && velocity.y.abs() < 0.01 && velocity.z.abs() < 0.01 {
-                    Vec3::ZERO
-                } else {
-                    velocity
-                };
+            let velocity = controller.intent * controller.max_speed;
+            let velocity = if velocity.x.abs() < 0.01 && velocity.y.abs() < 0.01 && velocity.z.abs() < 0.01 {
+                Vec3::ZERO
+            } else {
+                velocity
+            };
 
-                let world_position = controller_position.0.0;
+            let world_position = controller_position.0.0;
 
-                // Position we intend to move to
-                let intended_center_position = world_position + velocity * time.delta_secs();
+            // Position we intend to move to
+            let intended_center_position = world_position + velocity * time.delta_secs();
 
-                // Get collider information
-                let (collider_offset_x, collider_offset_z) = match collider.0 {
-                    ColliderType::Rectangle(size) => (size.x, size.z),
-                    ColliderType::Cylinder { radius, .. } => (radius, radius),
-                };
-                let direction_x = velocity.x.signum();
-                let direction_z = velocity.z.signum();
+            // Get collider information
+            let (collider_offset_x, collider_offset_z) = match collider.0 {
+                ColliderType::Rectangle(size) => (size.x, size.z),
+                ColliderType::Cylinder { radius, .. } => (radius, radius),
+            };
+            let direction_x = velocity.x.signum();
+            let direction_z = velocity.z.signum();
 
-                // Leading collider edge position
-                let intended_collider_edge_position_x = world_position + velocity * Vec3::X * time.delta_secs() + Vec3::new(collider_offset_x * direction_x, 0.0, 0.0);
-                let intended_collider_edge_position_z = world_position + velocity * Vec3::Z * time.delta_secs() + Vec3::new(0.0, 0.0, collider_offset_z * direction_z);
+            // Leading collider edge position
+            let intended_collider_edge_position_x = world_position + velocity * Vec3::X * time.delta_secs() + Vec3::new(collider_offset_x, 0.0, 0.0) * direction_x;
+            let intended_collider_edge_position_z = world_position + velocity * Vec3::Z * time.delta_secs() + Vec3::new(0.0, 0.0, collider_offset_z) * direction_z;
 
-                // Actual position we will move to
-                let mut final_position = world_position;
+            // Actual position we will move to
+            let mut final_position = world_position;
 
-                final_position.y = world_position.y;
+            let mut target_height = world_position.y;
 
-                // Set height to collision height of current tile
-                let current_tile = tile_map.get(&world_position.into());
-                if let Some(tile) = current_tile {
-                    for (entity, tile_collision) in tile_query.iter() {
-                        if &entity == tile {
-                            let collision_height = tile_collision.get_height(intended_center_position.x, intended_center_position.z) + world_position.y - 1.0;
-                            final_position.y = collision_height;
-                        }
+            // Set height to collision height of current tile
+            let current_tile = tile_map.get(&world_position.into());
+            if let Some(tile) = current_tile {
+                for (entity, tile_collision) in tile_query.iter() {
+                    if &entity == tile {
+                        let collision_height = tile_collision.get_height(intended_center_position.x, intended_center_position.z) + world_position.y - 1.0;
+                        target_height = collision_height;
                     }
                 }
-
-                // For X axis:
-                let final_pos_x = check_axis_movement(
-                    grid,
-                    world_position,
-                    intended_center_position,
-                    intended_collider_edge_position_x,
-                    collider_offset_x,
-                    direction_x,
-                    Vec3::X,
-                    tile_query
-                );
-
-                // For Z axis:
-                let final_pos_z = check_axis_movement(
-                    grid,
-                    world_position,
-                    intended_center_position,
-                    intended_collider_edge_position_z,
-                    collider_offset_z,
-                    direction_z,
-                    Vec3::Z,
-                    tile_query
-                );
-
-                final_position.x = final_pos_x.x;
-                final_position.z = final_pos_z.z;
-
-                final_position.y = final_pos_x.y.max(final_pos_z.y);
-
-                controller_position.0.0 = final_position;
             }
+
+            if world_position.y - target_height > GRAVITY * time.delta_secs() {
+                final_position.y = world_position.y - GRAVITY * time.delta_secs();
+            } else {
+                final_position.y = target_height;
+            }
+
+            // For X axis:
+            let final_pos_x = check_axis_movement(
+                grid,
+                time.delta_secs(),
+                world_position,
+                intended_center_position,
+                intended_collider_edge_position_x,
+                collider_offset_x,
+                direction_x,
+                Vec3::X,
+                tile_query
+            );
+
+            // For Z axis:
+            let final_pos_z = check_axis_movement(
+                grid,
+                time.delta_secs(),
+                world_position,
+                intended_center_position,
+                intended_collider_edge_position_z,
+                collider_offset_z,
+                direction_z,
+                Vec3::Z,
+                tile_query
+            );
+
+            final_position.x = final_pos_x.x;
+            final_position.z = final_pos_z.z;
+
+            final_position.y = final_pos_x.y.max(final_pos_z.y);
+
+            controller_position.0.0 = final_position;
         }
     }
 }
 
 fn check_axis_movement(
     grid: &Grid,
+    delta_secs: f32,
     world_position: Vec3,
     intended_center_position: Vec3,
     intended_collider_edge_position: Vec3,
@@ -188,22 +195,24 @@ fn check_axis_movement(
     ).into();
     let test_collider_tile_above = tile_map.get(&test_collider_position_above.0.into());
 
+    let mut target_height = world_position.y;
+
     let moved = match (test_collider_tile, test_tile_above, test_collider_tile_above) {
         // There is something for us to run into or to walk onto
-        (_, Some(tile_above), _) => {
+        (Some(_), Some(tile_above), _) => {
             if let Ok((_, tile_collision)) = tile_query.get(*tile_above) {
                 // Check collision
                 let test_height = tile_collision.get_height(intended_center_position.x, intended_center_position.z) + test_position_above.y as f32 - 1.0;
                 if test_height.max(0.0).min(1.0) <= world_position.y + STEP_UP_THRESHOLD {
                     final_position.x = intended_center_position.x;
                     final_position.z = intended_center_position.z;
-                    final_position.y = test_height;
+                    target_height = test_height;
                     true
                 } else { false }
             } else { false }
         }
         // We are approaching something to run into or to walk onto but haven't reached it yet
-        (_, _, Some(collider_tile_above)) => {
+        (Some(_), _, Some(collider_tile_above)) => {
             if let Ok((_, tile_collision)) = tile_query.get(*collider_tile_above) {
                 // Check collision
                 let test_height = tile_collision.get_height(intended_collider_edge_position.x, intended_collider_edge_position.z) + test_position_above.y as f32 - 1.0;
@@ -211,7 +220,6 @@ fn check_axis_movement(
                 if test_height <= world_position.y + STEP_UP_THRESHOLD {
                     final_position.x = intended_center_position.x;
                     final_position.z = intended_center_position.z;
-                    //final_position.y = target_height;
                     true
                 } else { false }
             } else { false }
@@ -224,7 +232,7 @@ fn check_axis_movement(
                 if test_height <= world_position.y + STEP_UP_THRESHOLD {
                     final_position.x = intended_center_position.x;
                     final_position.z = intended_center_position.z;
-                    final_position.y = test_height;
+                    target_height = test_height;
                     true
                 } else { false }
             } else { false }
@@ -248,6 +256,16 @@ fn check_axis_movement(
         };
 
         final_position = final_position + axis_mask * (clamped_coord - (world_position * axis_mask).dot(axis_mask));
+    }
+
+    println!("Target height: {:?}, current height: {:?}", target_height, world_position.y);
+    println!("Diff: {:?}", world_position.y - target_height);
+    println!("Gravity: {:?}", GRAVITY * delta_secs);
+
+    if world_position.y - target_height > GRAVITY * delta_secs {
+        final_position.y = world_position.y - GRAVITY * delta_secs;
+    } else {
+        final_position.y = target_height;
     }
 
     final_position
